caa77512e57f05db60a5d48469431da115d2fd78
BryanCPruett <byancpruett@gmail.com>
2019-12-05 19:40:15 -0500

    Adding LinkedNode

---
+++ LinkedNode.cpp
@@ -0,0 +1,29 @@
+//
+// Created by Toby Dragon on 10/17/17.
+//
+#include "LinkedNode.h"
+
+LinkedNode::LinkedNode(Song item){
+    this->item = item;
+    next = nullptr;
+}
+
+LinkedNode::LinkedNode(const LinkedNode& nodeToCopy){
+    item = nodeToCopy.item;
+    next = nullptr;
+}
+Song LinkedNode::getItem(){
+    return item;
+}
+
+LinkedNode* LinkedNode::getNext(){
+    return next;
+}
+
+void LinkedNode::setItem(Song newItem){
+    item = newItem;
+}
+
+void LinkedNode::setNext(LinkedNode* newNext){
+    next = newNext;
+}
