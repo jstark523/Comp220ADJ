4aacaa0af2c7cc9193a2a8358f9abb973facbf13
jstark523 <nospaminmain@gmail.com>
2019-12-13 23:03:54 -0500

    Updates

--- Sort.cpp
+++ Sort.cpp
@@ -2,16 +2,20 @@
 // Created by faste on 12/13/2019.
 //
 
-#include<iostream>
+#include <iostream>
 #include <string>
+#include "SongNode.h"
+#include "LinkedQueueSong.h"
+#include "Sort.h"
+#include "SongStorage.h"
 
-void swap(std::string *xp, std::string *yp){
+void Sort::swap(std::string *xp, std::string *yp){
     std::string  temp= *xp;
     *xp= *yp;
     *yp= temp;
 }
 
-void bubbleSort(std::string arr[], int n) {
+void Sort::bubbleSort(std::string arr[], int n) {
     int i, j;
     for (i = 0; i < n - 1; i++) {
         for (j = 0; j < n - i - 1; j++) {
@@ -21,19 +25,33 @@ void bubbleSort(std::string arr[], int n) {
         }
     }
 }
-void printArray(std::string arr[], int size){
+void Sort::printArray(std::string arr[], int size){
         int i;
         for(i=0; i < size; i++)
             std::cout << arr[i] << std::endl;
         std::cout << std::endl;
 }
 
-int main(){
-    std::string arr[]= {"Jon Bellion*Hand Of God*210*0","AJR*100 Bad Days*160*0", "AJR*Weak*200*0", "Jon Bellion*All Time Low*135*0", "Noah Kahan*Busyhead*180*0", "Jon Bellion*Fasion*160*0",
-     "Noah Kahan*Carlo's Song*190*0","AJR*Next Up Forever*180*0", "Noah Kahan*Young Blood*170*0"};
-    int n= sizeof(arr)/sizeof(arr[0]);
-    bubbleSort(arr, n);
-    std::cout<< "Sorted array: \n";
-    printArray(arr, n);
-    return 0;
+void Sort::sortLibrary(SongStorage* libraryIn){
+    if(libraryIn->getSongCount() > 0) {
+        std::string arr[libraryIn->getSongCount()];
+        SongNode *iterator = libraryIn->getSongList()->getFront();
+        Song tempSong = iterator->getItem();
+        std::string tempSongString = tempSong.songToString(tempSong);
+        arr[0] = tempSongString;
+        for (int i = 1; i < libraryIn->getSongCount(); i++) {
+            iterator = iterator->getNext();
+            tempSong = iterator->getItem();
+            tempSongString = tempSong.songToString(tempSong);
+            arr[i] = tempSongString;
+        }
+        int n = sizeof(arr) / sizeof(arr[0]);
+        bubbleSort(arr, n);
+        std::cout << "Sorted array: \n";
+        printArray(arr, n);
+    }
+
+    else{
+        std::cout<<"Library has no songs"<<std::endl;
+    }
 }
